65429c51e74ddd9ef3c22568b4de995a0fb84deb
chenshuai <15811033103@163.com>
2021-12-29 21:31:46 +0800

    Revert "init"
    This reverts commit 312ba1bd9e039f0ead31e8a8b1ab8b43bc858a2d.
    
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/App.vue
    #	src/main.js
    #	src/router/index.js
    #	src/views/Home.vue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import '@/assets/css/global.css'
+import 'element-plus/dist/index.css'
+import  'dayjs/locale/zh-cn'
+import locale from 'element-plus/es/locale/lang/zh-cn'
+import axios from "axios";
+// 引入css的全局设置
 
-createApp(App).use(store).use(router).mount('#app')
+
+
+createApp(App).use(store).use(router).use(ElementPlus,{locale,size:'small'}).mount('#app')
